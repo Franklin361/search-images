c1d0dba4b931e4e2e2b5107d644fdc40ff67fcf9
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2022-09-16 11:44:24 -0500

    [FEAT]: wrap app with query client

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App'
 import './index.css'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} position='bottom-left' />
+    </QueryClientProvider>
   </React.StrictMode>
 )
